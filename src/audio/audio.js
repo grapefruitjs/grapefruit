a650a6e852259d3be0df5e93de264eea6c9950a5
Chad Engler <englercj@live.com>
2012-12-03 13:15:02 -0500

    added audio component

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -1,4 +1,8 @@
 (function() {
+    //the list of audio channels
+    var playing = {},
+        resetTime = 0;
+
     gf.audio = {
         //have we initialized the audio already?
         _initialized: false,
@@ -6,7 +10,48 @@
         init: function() {
             if(gf.audio._initialized) return;
 
+            //activeAudioExt = gf.audio.getSupportedAudioFormat(!!format ? format.toString() : 'mp3');
+
             gf.audio._initialized = true;
+        },
+        play: function(soundid, loop, cb) {
+            if(!gf.resources[soundid]) return this;
+
+            //paused
+            if(playing[soundid]) {
+                playing[soundid].play();
+                return this;
+            }
+
+            var sound = playing[soundid] = gf.resources[soundid].data;
+            sound.loop = loop || false;
+            sound.play();
+
+            if(!loop) {
+                sound.addEventListener('ended', function(e) {
+                    sound.removeEventListener('ended', arguments.callee, false);
+                    delete playing[soundid];
+                    if(cb) cb();
+                }, false);
+            }
+
+            return this;
+        },
+        stop: function(soundid) {
+            if(!playing[soundid]) return this;
+
+            playing[soundid].pause();
+            playing[soundid].currentTime = resetTime;
+            delete playing[soundid];
+
+            return this;
+        },
+        pause: function(soundid) {
+            if(!playing[soundid]) return this;
+
+            playing[soundid].pause();
+
+            return this;
         }
     };
 })();
