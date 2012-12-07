2a3413088c6584e19f3ed2131ccf647d0aeb4341
Chad Engler <englercj@live.com>
2012-12-07 14:58:28 -0500

    Got a basic HUD written

--- src/gui/gui.js
+++ src/gui/gui.js
@@ -9,47 +9,4 @@
             gf.gui._initialized = true;
         }
     };
-
-    gf.GuiObject = gf.SceneObject.extend({
-        init: function(pos, settings) {
-            //Are the coords the user will pass actually relative to the element (screenCoords)?
-            this.useScreenCoords = true;
-
-            /****************************************************************************
-             * Call base constructor
-             ****************************************************************************/
-            gf.SceneObject.prototype.setValues.call(this, settings);
-            //this._super(settings);
-
-            /****************************************************************************
-             * Create the actual object for the scene
-             ****************************************************************************/
-            //if the hitbox size isn't set, use the same as normal size
-            if(this.hitSize.x === 0 && this.hitSize.y === 0)
-                this.hitSize = this.size.clone();
-
-            //scale size vectors
-            this.scaledSize = this.size.clone().multiplyScalar(this.scale);
-            this.scaledHitSize = this.hitSize.clone().multiplyScalar(this.scale);
-            this.scaledHitOffset = this.hitOffset.clone().multiplyScalar(this.scale);
-
-            //create main entity mesh
-            this._createMesh();
-
-            //create a second mesh for the hitbox
-            this._createHitboxMesh();
-
-            //set default position
-            this.setPosition(pos);
-        },
-        _doSetPos: function(x, y, z) {
-            if(this.useScreenCoords) {
-                //transform the coords into world coords before setting the position
-                var pos = gf.utils.project.screenToPosition(new THREE.Vector2(x, y));
-                this._super(pos.x, pos.y, z);
-            } else {
-                this._super(x, y, z);
-            }
-        },
-    });
 })();
--- src/gui/hud.js
+++ src/gui/hud.js
@@ -1,9 +1,91 @@
 (function() {
-    gf.hud = {
-        init: function() {}
-    };
+    gf.HUD = {
+        init: function(settings) {
+            //background color
+            this.bgColor = 'rgba(0,0,0,0)'; //any valid css color notation works
 
-    gf.GuiObject = gf.Sprite.extend({
-        init: function(pos, x) {}
-    });
+            //is the HUD visible?
+            this.visible = true;
+
+            //use the passed settings object ot override the default values above
+            gf.utils.setValues(this, settings);
+
+            //create the base div of this element
+            this._createElement(x, y);
+
+            //some private stuffs
+            this.dirty = true;
+
+            //the items in this HUD
+            this.items = {};
+            this.numItems = 0;
+        },
+        addItem: function(name, item) {
+            //increment if a new item
+            if(!this.items[name]) this.numItems++;
+
+            this.items[name] = item;
+            this.dirty = true;
+            return this;
+        },
+        removeItem: function(name) {
+            if(this.items[name]) {
+                this.items[name] = null;
+                this.numItems--;
+                this.dirty = true;
+            }
+
+            return this;
+        },
+        setItemValue: function(name, value) {
+            if(this.items[name]) {
+                this.items[name].setValue;
+                this.dirty = true;
+            }
+
+            return this;
+        },
+        getItemValue: function(name) {
+            return (this.items[name] ? this.items[name].getValue() : null);
+        },
+        reset: function(name) {
+            if(name !== undefined) {
+                if(this.items[name]) this.items[name].reset();
+                this.dirty = true;
+            } else {
+                this.resetAll();
+            }
+
+            return this;
+        },
+        resetAll: function() {
+            gf.utils.each(this.items, function(name, item) {
+                item.reset();
+            });
+
+            return this;
+        },
+        update: function() {
+            if(!this.dirty) return;
+
+            if(this.bgColor) {
+                this.$elm.css('backgroundColor', this.bgColor);
+            }
+
+            gf.utils.each(this.items, function(name, item) {
+                if(item.visible) {
+                    item.update();
+                }
+            });
+
+            this.dirty = false;
+        },
+        _createElement: function() {
+            this.$elm = $('<div/>', {
+                position: 'absolute',
+                top: gf.game._$cont.pos().top,
+                left: gf.game._$cont.pos().left
+            }).prependTo(gf.game._$cont);
+        }
+    };
 })();
