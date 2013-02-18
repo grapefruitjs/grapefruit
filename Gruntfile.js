b88c35604541c93fcb1c6b5d90d4a9eff767d759
Chad Engler <Chad.Engler@patlive.com>
2013-02-18 13:59:46 -0500

    Usage grunt v0.4.0 and add package.json file

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    var banner = [
+        '/**',
+        ' * @license',
+        ' * <%= pkg.longName %> - v<%= pkg.version %>',
+        ' * Copyright (c) 2012, Chad Engler',
+        ' * <%= pkg.homepage %>',
+        ' *',
+        ' * Compiled: <%= grunt.template.today("yyyy-mm-dd") %>',
+        ' *',
+        ' * <%= pkg.longName %> is licensed under the <%= pkg.license %> License.',
+        ' * <%= pkg.licenseUrl %>',
+        ' */',
+        ''
+    ].join('\n');
+
+    //Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dirs: {
+            build: 'build',
+            docs: 'docs',
+            src: 'src',
+            test: 'test',
+            tools: 'tools',
+            vendor: 'vendor'
+        },
+        files: {
+            vendor: '<%= dirs.vendor %>/**/*js',
+            core: 'core.js',
+            components: '<%= dirs.src %>/**/*.js',
+            intro: '<%= dirs.src %>/intro.js',
+            outro: '<%= dirs.src %>/outro.js',
+            build: '<%= dirs.build %>/<%= pkg.name %>.js',
+            buildMin: '<%= dirs.build %>/<%= pkg.name %>.min.js'
+        },
+        concat: {
+            options: {
+                banner: banner
+            },
+            dist: {
+                src: ['<%= files.intro %>', '<%= files.vendor %>', '<%= files.core %>', '<%= files.components %>', '<%= files.outro %>'],
+                dest: '<%= files.build %>'
+            }
+        },
+        uglify: {
+            options: {
+                banner: banner
+            },
+            dist: {
+                src: '<%= files.build %>',
+                dest: '<%= files.buildMin %>'
+            }
+        },
+        jshint: {
+            options: {
+                browser: true
+            }
+        }
+    });
+
+    //Load tasks
+    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('build', ['concat', 'uglify']);
+};
