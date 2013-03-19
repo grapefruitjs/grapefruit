0556ded1ec77b996c6e449bf448c669ba72e8f31
Chad Engler <Chad.Engler@patlive.com>
2013-03-19 17:55:17 -0400

    new build

--- build/gf.js
+++ build/gf.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2012, Chad Engler
  * https://github.com/englercj/grapefruit
  *
- * Compiled: 2013-02-19
+ * Compiled: 2013-03-19
  *
  * GrapeFruit Game Engine is licensed under the MIT License.
  * http://www.opensource.org/licenses/mit-license.php
@@ -1926,7 +1926,32 @@ Class.extend = function(prop) {
 (function() {
     //the list of audio channels
     var playing = {},
-        resetTime = 0;
+        resetTime = 0,
+        settings = {
+            loop: false,
+            volume: 1.0
+        };
+
+    function getOpen(id) {
+        var chans = playing[id];
+
+        //find an open channel
+        for(var i = 0, clip; clip = chans[i++];) {
+            if(clip.ended || !clip.currentTime) {
+                clip.currentTime = resetTime;
+                return clip;
+            }
+        }
+
+        //create a new channel
+        var sound = new Audio(chans[0].src);
+        sound.preload = 'auto';
+        sound.load();
+        sound.channel = chans.length;
+        chans.push(sound);
+
+        return chans[chans.length - 1];
+    }
 
     gf.audio = {
         //have we initialized the audio already?
@@ -1935,81 +1960,106 @@ Class.extend = function(prop) {
         init: function() {
             if(gf.audio._initialized) return;
 
-            //activeAudioExt = gf.audio.getSupportedAudioFormat(!!format ? format.toString() : 'mp3');
-
             gf.audio._initialized = true;
         },
-        play: function(soundid, opts, cb) {
-            if(!gf.resources[soundid]) return this;
+        play: function(id, opts, cb) {
+            if(!gf.resources[id]) {
+                console.warn('Tried to play unloaded audio: %s', id);
+                return false;
+            }
 
             if(typeof opts == 'function') {
                 cb = opts;
                 opts = null;
             }
 
-            opts = opts || {};
-
-            //paused
-            if(playing[soundid]) {
-                playing[soundid].play();
-                return this;
+            if(typeof id == 'object') {
+                opts = id;
+                id = opts.id;
             }
 
-            var sound = playing[soundid] = gf.resources[soundid].data;
-            sound.loop = opts.loop || false;
-            sound.volume = opts.volume || 1.0;
+            opts = opts || {};
+
+            opts.id = id;
+            opts.loop = opts.loop || settings.loop;
+            opts.volume = opts.volume || settings.volume
+
+            //resume a paused channel
+            if(opts.channel !== undefined && playing[id]) {
+                playing[id][opts.channel].play();
+                return opts;
+            }
+
+            //we haven't played this sound yet, create a new channel list
+            if(!playing[id]) {
+                playing[id] = [gf.resources[id].data];
+                playing[id].channel = 0;
+            }
+
+            var sound = getOpen(id);
+            sound.loop = opts.loop;
+            sound.volume = opts.volume;
             sound.play();
+
+            opts.channel = sound.channel;
 
             if(!opts.loop) {
                 sound.addEventListener('ended', function(e) {
                     sound.removeEventListener('ended', arguments.callee, false);
-                    sound.pause();
-                    sound.currentTime = resetTime;
-                    delete playing[soundid];
+                    gf.audio.stop(opts);
                     if(cb) cb();
                 }, false);
             }
 
-            return this;
+            return opts;
         },
-        stop: function(soundid) {
-            if(!playing[soundid]) return this;
+        stop: function(id, channel) {
+            if(typeof id == 'object') {
+                channel = id.channel;
+                id = id.id;
+            }
 
-            playing[soundid].pause();
-            playing[soundid].currentTime = resetTime;
-            delete playing[soundid];
+            if(!playing[id]) return;
+            if(!playing[id][channel]) return;
 
-            return this;
+            playing[id][channel].pause();
+            playing[id][channel].currentTime = resetTime;
+            playing[id][channel].ended = true;
         },
-        stopAll: function() {
+        pause: function(id, channel) {
+            if(typeof id == 'object') {
+                channel = id.channel;
+                id = id.id;
+            }
+
+            if(!playing[id]) return;
+            if(!playing[id][channel]) return;
+
+            playing[id][channel].pause();
+        },
+        playAll: function() {
             for(var sid in playing) {
-                gf.audio.stop(sid);
+                var chans = playing[sid];
+
+                for(var i = 0, il = chans.length; i < il; ++i)
+                    gf.audio.play(sid, i);
             }
         },
-        pause: function(soundid) {
-            if(!playing[soundid]) return this;
-
-            playing[soundid].pause();
-
-            return this;
-        },
         stopAll: function() {
             for(var sid in playing) {
-                gf.audio.stop(sid);
+                var chans = playing[sid];
+
+                for(var i = 0, il = chans.length; i < il; ++i)
+                    gf.audio.stop(sid, i);
             }
         },
         pauseAll: function() {
             for(var sid in playing) {
-                gf.audio.pause(sid);
+                var chans = playing[sid];
+
+                for(var i = 0, il = chans.length; i < il; ++i)
+                    gf.audio.pause(sid, i);
             }
-        },
-        playAll: function() {
-            for(var sid in playing) {
-                gf.audio.play(sid);
-            }
-        },
-        isPlaying: function(soundid) {
-            return !!playing[soundid];
         }
     };
 })();
@@ -3420,7 +3470,7 @@ Class.extend = function(prop) {
         removeItem: function(name) {
             if(this.items[name]) {
                 this.items[name].elm.parentNode.removeChild(this.items[name].elm);
-                this.items[name] = null;
+                delete this.items[name];
                 this.numItems--;
                 this.dirty = true;
             }
@@ -3476,6 +3526,7 @@ Class.extend = function(prop) {
         }
     };
 })();
+
 (function() {
     gf.HudItem = Class.extend({
         init: function(x, y, settings) {
@@ -4803,7 +4854,7 @@ Class.extend = function(prop) {
             mesh.position.set(x || 0, y || 0, 400);
             gf.game._scene.add(mesh);
         },
-        numToHexColor: function(num) { return ("00000000" + num.toString(16)).substr(-8); },
+        numToHexColor: function(num) { return ('00000000' + num.toString(16)).substr(-8); },
         RGBToHex: function(r, g, b) { return r.toHex() + g.toHex() + b.toHex(); },
         noop: function() {},
         ajax: function(sets) {
