79d7ee35cc7b8d1eb19a02513c8317ec0dd00464
Chad Engler <englercj@live.com>
2013-03-29 10:46:33 -0400

    Fix intro/outro to work properly

--- src/intro.js
+++ src/intro.js
@@ -1,2 +1,4 @@
 (function(window, undefined) {
     document = window.document;
+    //shadow these so vendors export to global (hacky and dumb...)
+    var require = define = exports = module = undefined;
--- src/outro.js
+++ src/outro.js
@@ -1,12 +1,2 @@
 
-    if (typeof exports === 'object') {
-        // CommonJS
-        exports.gf = gf;
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(gf);
-    } else {
-        // Browser globals
-        window.gf = gf;
-    }
 })(window);
