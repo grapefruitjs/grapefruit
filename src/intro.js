9bc18c26d0893234609999e44cce35c09d51736b
Chad Engler <Chad.Engler@patlive.com>
2013-04-02 11:43:03 -0400

    remove require shadowing

--- src/intro.js
+++ src/intro.js
@@ -1,4 +1,2 @@
 (function(window, undefined) {
     document = window.document;
-    //shadow these so vendors export to global (hacky and dumb...)
-    var require = define = exports = module = undefined;
