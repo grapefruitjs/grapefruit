a3e2d5c1d5f3d566707b03943af1c3b5dda52909
Chad Engler <Chad.Engler@patlive.com>
2013-04-03 15:58:23 -0400

    initial start of Mapping rewrite

--- src/map/Layer.js
+++ src/map/Layer.js
@@ -12,7 +12,7 @@ gf.Layer = function(settings) {
      * @type String
      * @default ''
      */
-    this.name = settings.name || '';
+    this.name = '';
 
     /**
      * The size of the layer
@@ -21,7 +21,22 @@ gf.Layer = function(settings) {
      * @type Vector
      * @default new gf.Vector(1, 1)
      */
-    this.size = settings.size ? gf.utils.ensureVector(settings.size) : new gf.Vector(settings.width || 1, settings.height || 1);
+    this.size = new gf.Vector(settings.width || 0, settings.height || 0);
+
+    /**
+     * Texture of the layer
+     *
+     * @property texture
+     * @type Texture
+     * @default null
+     */
+    this.texture = null;
+
+    //call base ctor
+    PIXI.DisplayObjectContainer.call(this);
+
+    //mixin user's settings
+    gf.utils.setValues(this, settings);
 
     /**
      * Half of the size of the layer
@@ -31,93 +46,6 @@ gf.Layer = function(settings) {
      * @private
      */
     this.hSize = this.size.clone().divideScalar(2);
-
-    /**
-     * Offset of the layer
-     *
-     * @property offset
-     * @type Vector
-     * @default new gf.Vector(0, 0)
-     */
-    this.offset = settings.offset ? gf.utils.ensureVector(settings.offset) : new gf.Vector(settings.x || 0, settings.y || 0);
-
-    /**
-     * Scale of the layer
-     *
-     * @property scale
-     * @type Number
-     * @default 1
-     */
-    this.scale = settings.scale || 1;
-
-    /**
-     * Make the mesh wireframe
-     *
-     * @property wireframe
-     * @type Boolean
-     * @default false
-     */
-    this.wireframe = settings.wireframe || false;
-
-    /**
-     * Color of the layer
-     *
-     * @property color
-     * @type Number
-     * @default 0xffffff
-     */
-    this.color = (settings.color !== undefined ? settings.color : 0xffffff);
-
-    /**
-     * Texture of the layer
-     *
-     * @property texture
-     * @type Texture
-     * @default null
-     */
-    this.texture = settings.texture || null;
-
-    /**
-     * Opacity of the layer
-     *
-     * @property opacity
-     * @type Number
-     * @default 1
-     */
-    this.opacity = settings.opacity || 1;
-
-    /**
-     * The visibility of the layer
-     *
-     * @property visible
-     * @type Boolean
-     * @default true
-     */
-    this.visible = (settings.visible !== undefined ? settings.visible : true);
-
-    this._createMesh();
 };
 
-gf.inherits(gf.Layer, Object, {
-    _createMesh: function() {
-        if(this._mesh) return;
-
-        //create the shader material
-        this._material = new THREE.MeshBasicMaterial({
-            color: this.color,
-            wireframe: this.wireframe,
-            map: this.texture,
-            opacity: this.opacity,
-            transparent: (this.opacity !== 1) //if the opacity isn't 1.0, then this needs to be transparent
-        });
-
-        this._plane = new THREE.PlaneGeometry(
-            this.size.x * this.scale,
-            this.size.y * this.scale
-        );
-
-        this._mesh = new THREE.Mesh(this._plane, this._material);
-        this._mesh.visible = this.visible;
-        this._mesh.position.z = this.zIndex;
-    }
-});
+gf.inherits(gf.Layer, PIXI.DisplayObjectContainer);
--- src/map/Map.js
+++ src/map/Map.js
@@ -14,30 +14,14 @@ gf.Map = function(settings) {
      */
     this.size = new gf.Vector(settings.width, settings.height);
 
-    /**
-     * The layers of the map
-     *
-     * @property layers
-     * @type Vector
-     * @private
-     * @default []
-     */
-    this.layers = [];
+    //call base ctor
+    PIXI.DisplayObjectContainer.call(this);
+
+    //mixin user's settings
+    gf.utils.setValues(this, settings);
 };
 
-gf.inherits(gf.Map, Object, {
-    /**
-     * Adds a layer to the map
-     *
-     * @method addLayer
-     * @param layer {Layer}
-     * @return {Map} Returns itself for chainability
-     */
-    addLayer: function(layer) {
-        this.layers.push(layer);
-
-        return this;
-    },
+gf.inherits(gf.Map, PIXI.DisplayObjectContainer, {
     /**
      * Gets a layer based on the layer's id or name
      *
@@ -49,51 +33,11 @@ gf.inherits(gf.Map, Object, {
         if(typeof id === 'number')
             return this.layers[id] || null; //return null if not found
 
-        if(typeof id === 'string') {
-            var lyr = null;
-            this.eachLayer(function(layer) {
-                if(layer.name === id) {
-                    lyr = layer;
-                    return false;
-                }
-            });
-
-            return lyr;
-        }
+        if(typeof id === 'string')
+            for(var i = 0, il = this.children.length; i < il; ++i)
+                if(this.children[i].name === id)
+                    return this.children[i];
 
         return null;
-    },
-    /**
-     * Adds the map and all layers to the scene
-     *
-     * @method addToScene
-     * @param scene {gf.game._stage} The game stage to add everything to
-     * @return {Map} Returns itself for chainability
-     */
-    addToScene: function(scene) {
-        this.scene = scene;
-
-        //incase they add layers first, then add the map to the scene
-        this.eachLayer(function(layer) {
-            if(!layer.scene || layer.scene !== scene)
-                layer.addToScene(scene);
-        });
-
-        return this;
-    },
-    /**
-     * Applies an iterator function to each of the layers.
-     *
-     * @method eachLayer
-     * @param iterator {Function} The iterator to call for each layer in the Map
-     * @return {Map} Returns itself for chainability
-     */
-    eachLayer: function(fn) {
-        for(var i = 0, il = this.layers.length; i < il; ++i) {
-            if(fn.call(this, this.layers[i], i, this.layers) === false)
-                break;
-        }
-
-        return this;
     }
 });
