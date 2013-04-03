3e780564455d1563d8ac7f154bb487e2d7c83f8f
Chad Engler <Chad.Engler@patlive.com>
2013-04-03 10:21:10 -0400

    Add docs to Map

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,24 +1,53 @@
+/**
+ * Base Map implementation, provides common functions for all Map types
+ *
+ * @module gf
+ * @class audio
+ */
 gf.Map = function(settings) {
-    //size of the map
+    /**
+     * The size of the map
+     *
+     * @property size
+     * @type Vector
+     * @default new gf.Vector(0, 0)
+     */
     this.size = new gf.Vector(settings.width, settings.height);
 
-    //orientation of the map
-    this.orientation = settings.orientation; //only orthogonal supported
-
-    //layers of the map
+    /**
+     * The layers of the map
+     *
+     * @property layers
+     * @type Vector
+     * @private
+     * @default []
+     */
     this.layers = [];
 };
 
 gf.inherits(gf.Map, Object, {
-    //virtual add layer
+    /**
+     * Adds a layer to the map
+     *
+     * @method addLayer
+     * @param layer {Layer}
+     * @return {Map} Returns itself for chainability
+     */
     addLayer: function(layer) {
         this.layers.push(layer);
 
         return this;
     },
+    /**
+     * Gets a layer based on the layer's id or name
+     *
+     * @method getLayer
+     * @param id {Number|String} The layer's number id or string name.
+     * @return {Layer} Returns the found layer, or null if not found
+     */
     getLayer: function(id) {
         if(typeof id === 'number')
-            return this.layers[id];
+            return this.layers[id] || null; //return null if not found
 
         if(typeof id === 'string') {
             var lyr = null;
@@ -32,9 +61,15 @@ gf.inherits(gf.Map, Object, {
             return lyr;
         }
 
-        return this;
+        return null;
     },
-    //add all layers to the scene
+    /**
+     * Adds the map and all layers to the scene
+     *
+     * @method addToScene
+     * @param scene {gf.game._stage} The game stage to add everything to
+     * @return {Map} Returns itself for chainability
+     */
     addToScene: function(scene) {
         this.scene = scene;
 
@@ -46,7 +81,13 @@ gf.inherits(gf.Map, Object, {
 
         return this;
     },
-    //apply an iterator to each layer
+    /**
+     * Applies an iterator function to each of the layers.
+     *
+     * @method eachLayer
+     * @param iterator {Function} The iterator to call for each layer in the Map
+     * @return {Map} Returns itself for chainability
+     */
     eachLayer: function(fn) {
         for(var i = 0, il = this.layers.length; i < il; ++i) {
             if(fn.call(this, this.layers[i], i, this.layers) === false)
