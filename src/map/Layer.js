b3183e22d168fa9e783170c369eccde0138faa66
Chad Engler <Chad.Engler@patlive.com>
2013-04-03 11:38:20 -0400

    Add some docs to Layer

--- src/map/Layer.js
+++ src/map/Layer.js
@@ -1,30 +1,98 @@
+/**
+ * Base Layer implementation, provides common functions for all Layer types
+ *
+ * @module gf
+ * @class Layer
+ */
 gf.Layer = function(settings) {
-    //name of this layer
+    /**
+     * The name of the layer
+     *
+     * @property name
+     * @type String
+     * @default ''
+     */
     this.name = settings.name || '';
 
-    //size of this layer
-    this.size = new gf.Vector(settings.width || 1, settings.height || 1);
+    /**
+     * The size of the layer
+     *
+     * @property size
+     * @type Vector
+     * @default new gf.Vector(1, 1)
+     */
+    this.size = settings.size ? gf.utils.ensureVector(settings.size) : new gf.Vector(settings.width || 1, settings.height || 1);
 
-    //half size
+    /**
+     * Half of the size of the layer
+     *
+     * @property hSize
+     * @type Vector
+     * @private
+     */
     this.hSize = this.size.clone().divideScalar(2);
 
-    //offset of this layer
-    this.offset = new gf.Vector(settings.x || 0, settings.y || 0);
+    /**
+     * Offset of the layer
+     *
+     * @property offset
+     * @type Vector
+     * @default new gf.Vector(0, 0)
+     */
+    this.offset = settings.offset ? gf.utils.ensureVector(settings.offset) : new gf.Vector(settings.x || 0, settings.y || 0);
 
-    //scale of this layer
+    /**
+     * Scale of the layer
+     *
+     * @property scale
+     * @type Number
+     * @default 1
+     */
     this.scale = settings.scale || 1;
 
-    //wireframe this mesh?
+    /**
+     * Make the mesh wireframe
+     *
+     * @property wireframe
+     * @type Boolean
+     * @default false
+     */
     this.wireframe = settings.wireframe || false;
 
-    //color of this layer
+    /**
+     * Color of the layer
+     *
+     * @property color
+     * @type Number
+     * @default 0xffffff
+     */
     this.color = (settings.color !== undefined ? settings.color : 0xffffff);
 
-    //texture of the layer
+    /**
+     * Texture of the layer
+     *
+     * @property texture
+     * @type Texture
+     * @default null
+     */
     this.texture = settings.texture || null;
 
-    //opacity of this layer
+    /**
+     * Opacity of the layer
+     *
+     * @property opacity
+     * @type Number
+     * @default 1
+     */
     this.opacity = settings.opacity || 1;
+
+    /**
+     * The visibility of the layer
+     *
+     * @property visible
+     * @type Boolean
+     * @default true
+     */
     this.visible = (settings.visible !== undefined ? settings.visible : true);
 
     this._createMesh();
