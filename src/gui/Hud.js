831c93e8d36f8aa75f6b4bf87840f12ff56bd0e4
Chad Engler <englercj@live.com>
2013-04-08 20:41:40 -0400

    working on gui and hud

---
+++ src/gui/Hud.js
@@ -0,0 +1,15 @@
+/**
+ * The Hud that holds HudItems to be presented as a Hud
+ *
+ * @module gf
+ * @class Hud
+ * @extends DisplayObject
+ * @constructor
+ * @param pos {Array|Vector|Point} The starting position of the sprite
+ * @param settings {Object} Settings to override the defauls
+ */
+gf.Hud = function(pos, settings) {
+    gf.Gui.call(this, pos, settings);
+};
+
+gf.inherits(gf.Hud, gf.Gui);
