23a93835505b2b47b175f5b888283171968da02a
Chad Engler <englercj@live.com>
2013-04-08 20:41:40 -0400

    working on gui and hud

--- src/gui/GuiItem.js
+++ src/gui/GuiItem.js
@@ -2,16 +2,34 @@
  * The base GuiItem that represents an element of a gui on the screen.
  *
  * @module gf
- * @class GuiElement
+ * @class GuiItem
  * @extends Sprite
  * @constructor
  * @param pos {Array|Vector|Point} The starting position of the sprite
  * @param settings {Object} Settings to override the defauls
  */
 gf.GuiItem = function(pos, settings) {
+    /**
+     * Whether or not the item needs an update
+     *
+     * @property dirty
+     * @type Boolean
+     * @default true
+     */
+    this.dirty = true;
+
     //allow user to pass the sprite texture as "image" to a GuiItem.
     settings.texture = settings.texture || settings.image;
     gf.Sprite.call(this, pos, settings);
 };
 
-gf.inherits(gf.GuiItem, gf.Sprite);
+gf.inherits(gf.GuiItem, gf.Sprite, {
+    /**
+     * Overrides base update to do some calculations. Called internally on each frame
+     *
+     * @method update
+     */
+    update: function() {
+        gf.Sprite.prototype.update.call(this);
+    }
+});
---
+++ src/gui/HudItem.js
@@ -0,0 +1,104 @@
+/**
+ * The base HudItem that represents an element of a hud on the screen.
+ *
+ * @module gf
+ * @class HudItem
+ * @extends GuiItem
+ * @constructor
+ * @param pos {Array|Vector|Point} The starting position of the sprite
+ * @param settings {Object} Settings to override the defauls
+ */
+gf.HudItem = function(pos, settings) {
+    /**
+     * The value of the item
+     *
+     * @property name
+     * @type Mixed
+     * @default ''
+     */
+    this.value = '';
+
+    /**
+     * Sets whether or not you can drag the HudItem around
+     *
+     * @property draggable
+     * @type Boolean
+     * @default false
+     */
+    this.draggable = false;
+
+    /**
+     * [read only] Describes if the current item is being dragged or not
+     *
+     * @property dragging
+     * @type Boolean
+     * @default false
+     * @readOnly
+     */
+    this.dragging = false;
+
+    gf.GuiItem.call(this, pos, settings);
+
+
+    /**
+     * The initial value of the item to reset to
+     *
+     * @property initialValue
+     * @type Mixed
+     */
+    this.initialValue = this.value;
+};
+
+gf.inherits(gf.HudItem, gf.GuiItem, {
+    /**
+     * Resets the value to the initialValue
+     *
+     * @method reset
+     * @return {HudItem} Returns itself for chainability
+     */
+    reset: function() {
+        return this.set(this.initialValue);
+    },
+    /**
+     * Sets the value of the item, and marks it as dirty
+     *
+     * @method set
+     * @return {HudItem} Returns itself for chainability
+     */
+    set: function(val) {
+        this.value = val;
+        this.dirty = true;
+        return this;
+    },
+
+    onDragStart: function() {},
+    onDragEnd: function() {},
+
+    click: function() {},
+    mousedown: function(e) {
+        if(!this.draggable) return;
+
+        this.dragging = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        this.onDragStart(e);
+    },
+    mouseup: function(e) {
+        this.dragging = false;
+        this.onDragEnd(e);
+    },
+    mousemove: function(e) {
+        if(!this.draggable || !this.dragging) return;
+
+        var diffX = e.clientX - this.dragging.x,
+            diffY = e.clientY - this.dragging.y,
+            pos = gf.utils.getPosition(this.elm);
+
+        this.dragging.x = e.clientX;
+        this.dragging.y = e.clientY;
+
+        this.elm.style.top = pos.top + diffY;
+        this.elm.style.left = pos.left + diffX;
+    }
+});
