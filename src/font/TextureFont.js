37e4c2a5fb19100bdcf7cf02b911b7ea2946a77d
Chad Engler <Chad.Engler@patlive.com>
2013-04-12 12:39:26 -0400

    initial font implementation

---
+++ src/font/TextureFont.js
@@ -0,0 +1,77 @@
+gf.TextureFont = function(font, settings) {
+    this.text = '';
+
+    this.lineWidth = 1;
+
+    this.lineHeight = 1;
+
+    this.ext = '';
+
+    gf.DisplayObject.call(this, font, settings);
+
+    if(typeof font === 'string') {
+        if(gf.assetCache[font])
+            font = gf.assetCache[font];
+    }
+
+    this.dirty = true;
+    this.textures = font;
+
+    if(this.ext && this.ext.charAt(0) !== '.')
+        this.ext = '.' + this.ext;
+};
+
+gf.inherits(gf.TextureFont, gf.DisplayObject, {
+    setText: function(txt) {
+        this.text = txt;
+        this.dirty = true;
+    },
+    _getSprite: function(ch) {
+        var texture = this.textures[ch + this.ext],
+            spr = this.sprites.pop();
+
+        if(!spr)
+            spr = new PIXI.Sprite(texture);
+        else
+            spr.setTexture(texture);
+
+        this.addChild(spr);
+    },
+    _freeSprite: function(spr) {
+        this.sprites.push(spr);
+        this.removeChild(spr);
+    },
+    update: function() {
+        if(!this.dirty) return;
+
+        //free all sprites
+        for(var c = this.children.length; c > -1; --c)
+            this._freeSprite(this.children[c]);
+
+        //add text sprites
+        var strs = this.text.split('\n'),
+            w = this.size.x * this.lineWidth,
+            h = this.size.y * this.lineHeight,
+            x = 0,
+            y = 0;
+
+        for(var i = 0, il = strs.length; i < il; ++i) {
+            var str = strs[i];
+
+            //create the string sprites
+            for(var s = 0, sl = str.length; s < sl; ++s) {
+                var ch = str.charAt(s),
+                    spr = this._getSprite(ch);
+
+                spr.position.x = x;
+                spr.position.y = y;
+
+                x += w;
+            }
+
+            y += h;
+        }
+
+        this.dirty = false;
+    }
+});
