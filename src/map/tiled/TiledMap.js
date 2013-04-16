b2ffac8a2fb6ecf3cbd042188bde06a9ec930c4e
Chad Engler <Chad.Engler@patlive.com>
2013-04-16 18:10:11 -0400

    Add some debug, and got single-point collisions working

--- src/map/tiled/TiledMap.js
+++ src/map/tiled/TiledMap.js
@@ -166,83 +166,60 @@ gf.inherits(gf.TiledMap, gf.Map, {
      */
     //see: http://stackoverflow.com/questions/2576412/tile-map-collision-detection
     checkCollision: function(ent, pv) {
-        if(!this.collisionLayer || !this.collisionTileset) return [];
+        if(!this.collisionLayer || !this.collisionTileset || (pv.x === 0 && pv.y === 0))
+            return [];
 
-            //get direction and normalize
-        var dir = pv.clone().normalize(),
+        if(gf.debug._showColliders && !this.sprites) {
+            this.sprites = new gf.ObjectPool(PIXI.Sprite, this);
+        }
+
+            //get movement vector and normalize as our step
+        var step = pv.clone().normalize(),
+            //starting position
+            start = ent.position,
             //end location
             end = new gf.Point(
-                (ent.position.x + pv.x),
-                (ent.position.y + pv.y)
-            ),
-            //the distance between 2 consectutive vertical lines
-            tDelta = new gf.Vector(
-                this.scaledTileSize.x / Math.abs(dir.x),
-                this.scaledTileSize.y / Math.abs(dir.y)
+                (start.x + pv.x),
+                (start.y + pv.y)
             ),
             //original cell location
             cell = new gf.Point(
-                ent.position.x / this.scaledTileSize.x,
-                ent.position.y / this.scaledTileSize.y
+                Math.floor(start.x / this.tileSize.x),
+                Math.floor(start.y / this.tileSize.y)
             ),
             //end cell
             endCell = new gf.Point(
-                end.x / this.scaledTileSize.x,
-                end.y / this.scaledTileSize.y
+                Math.floor(end.x / this.tileSize.x),
+                Math.floor(end.y / this.tileSize.y)
             ),
-            //how to move between tiles
-            step = new gf.Vector(),
-            tMax = new gf.Vector(),
-            blk = new gf.Vector(),
+            //the distance between 2 consectutive vertical lines
+            tDelta = new gf.Vector(
+                this.tileSize.x / Math.abs(step.x),
+                this.tileSize.y / Math.abs(step.y)
+            ),
             //temp and return vars
+            text = null,
+            tMax = new gf.Point(),
+            id = 0,
             tile = null,
             res = [];
 
-        //check for max entent
-        /*
-        if(x <= 0 || x >= this.realSize.x) {
-            res.push({ axis: 'x', tile: { type: gf.Layer.COLLISION.SOLID } });
-        }
-        if(y <= 0 || y >= this.realSize.y) {
-            res.push({ axis: 'y', tile: { type: gf.Layer.COLLISION.SOLID } });
-        }
-
-        if(res.length) return res;
-        */
-
-        //determine step and tMax, defined as:
-        //step: Determine in what way do we move between cells
-        //tMax: the distance in terms of vector(dirX,dirY) to the next vertical line
-        if(end.x > ent.position.x) {
-            blk.x = 0;
-            step.x = 1;
-            tMax.x = dir.x === 0 ? 0 : ((cell.x + 1) * this.scaledTileSize.x - ent.position.x) / dir.x;
+        if(end.x > start.x) {
+            tMax.x = step.x === 0 ? 0 : ((cell.x + 1) * this.tileSize.x - start.x) / step.x;
         } else {
-            blk.x = 1;
-            step.x = -1;
-            tMax.x = dir.x === 0 ? 0 : (cell.x * this.scaledTileSize.x - ent.position.x) / dir.x;
+            tMax.x = step.x === 0 ? 0 : (cell.x * this.tileSize.x - start.x) / step.x;
         }
 
-        if(end.y > ent.position.y) {
-            blk.y = 0;
-            step.y = 1;
-            tMax.y = dir.y === 0 ? 0 : ((cell.y + 1) * this.scaledTileSize.y - ent.position.y) / dir.y;
+        if(end.y > start.y) {
+            tMax.y = step.y === 0 ? 0 : ((cell.y + 1) * this.tileSize.y - start.y) / step.y;
         } else {
-            blk.y = 1;
-            step.y = -1;
-            tMax.y = dir.y === 0 ? 0 : (cell.y * this.scaledTileSize.y - ent.position.y) / dir.y;
+            tMax.y = step.y === 0 ? 0 : (cell.y * this.tileSize.y - start.y) / step.y;
         }
 
-        window.console.log('dir', dir);
-        window.console.log('start', ent.position);
-        window.console.log('end', end);
-        window.console.log('cell', cell);
-        window.console.log('endCell', endCell);
-        window.console.log('blk', blk);
-        window.console.log('step', step);
-        window.console.log('tMax', tMax);
-        window.console.log('tilesize', this.scaledTileSize);
-        window.console.log('pv', pv);
+        //ceil afterwards so tDelta and tMax are correct
+        step.x = step.x < 0 ? Math.floor(step.x) : Math.ceil(step.x);
+        step.y = step.y < 0 ? Math.floor(step.y) : Math.ceil(step.y);
+
         //check if we are on a colliding tile
         tile = this.collisionTileset.getTileProperties(this.collisionLayer.tiles[(cell.x + (cell.y * this.collisionLayer.size.x))]);
         if(tile && tile.isCollidable) {
@@ -251,19 +228,55 @@ gf.inherits(gf.TiledMap, gf.Map, {
             return res;
         }
 
+        //collider overlays
+        if(gf.debug._showColliders) {
+            this.sprites.freeAll();
+            for(var s = 0; s < this.sprites.pool.length; ++s)
+                this.sprites.pool[s].visible = false;
+        }
+
+        var spr;
         //scan all the tiles along the movement vector
         while(cell.x !== endCell.x || cell.y !== endCell.y) {
             if(tMax.x < tMax.y) {
                 tMax.x += tDelta.x;
                 cell.x += step.x;
-                tile = this.collisionTileset.getTileProperties(this.collisionLayer.tiles[(cell.x + (cell.y * this.collisionLayer.size.x))]);
+                id = this.collisionLayer.tiles[(cell.x + (cell.y * this.collisionLayer.size.x))];
+
+                if(gf.debug._showColliders) {
+                    text = this.collisionTileset.getTileTexture(id);
+                    if(text) {
+                        spr = this.sprites.create(text);
+                        spr.position.x = cell.x * this.tileSize.x;
+                        spr.position.y = cell.y * this.tileSize.y;
+                        spr.alpha = 0.5;
+                        spr.visible = true;
+                        spr.setTexture(text);
+                    }
+                }
+
+                tile = this.collisionTileset.getTileProperties(id);
                 if(tile && tile.isCollidable) {
                     res.push({ axis: 'x', tile: tile });
                 }
             } else {
                 tMax.y += tDelta.y;
                 cell.y += step.y;
-                tile = this.collisionTileset.getTileProperties(this.collisionLayer.tiles[(cell.x + (cell.y * this.collisionLayer.size.x))]);
+                id = this.collisionLayer.tiles[(cell.x + (cell.y * this.collisionLayer.size.x))];
+
+                if(gf.debug._showColliders) {
+                    text = this.collisionTileset.getTileTexture(id);
+                    if(text) {
+                        spr = this.sprites.create(text);
+                        spr.position.x = cell.x * this.tileSize.x;
+                        spr.position.y = cell.y * this.tileSize.y;
+                        spr.alpha = 0.5;
+                        spr.visible = true;
+                        spr.setTexture(text);
+                    }
+                }
+
+                tile = this.collisionTileset.getTileProperties(id);
                 if(tile && tile.isCollidable) {
                     res.push({ axis: 'y', tile: tile });
                 }
