bf32b8010d1da0760c4f57e21f0e03c098b675f2
Chad Engler <cengler@blizzard.com>
2013-06-04 17:45:40 -0700

    actually update entities based on physics results

--- src/physics/PhysicsSystem.js
+++ src/physics/PhysicsSystem.js
@@ -19,6 +19,29 @@ gf.PhysicsSystem = function(game, gravity, profile) {
             };
         }
     });
+
+    this.children = [];
+};
+
+gf.PhysicsSystem.prototype.addEntity = function(ent) {
+    var shape = new p2.Plane(),
+        body = new p2.Body({
+            shape: shape,
+            mass: ent.mass,
+            angle: ent.rotation,
+            position: ent.position
+        });
+
+    this.addBody(body);
+    this.children.push([ent, body]);
+};
+
+gf.PhysicsSystem.prototype.removeEntity = function(ent) {
+    var id = this.findIdByEntity(ent);
+
+    //if we found one, remove it
+    if(id >= 0)
+        this.children.splice(id, 1);
 };
 
 gf.PhysicsSystem.prototype.defineGrid = function(numTiles, tileSize) {
@@ -30,4 +53,52 @@ gf.PhysicsSystem.prototype.defineGrid = function(numTiles, tileSize) {
         numTiles.x * tileSize.x,    //num x
         numTiles.y * tileSize.y     //num y
     );
+};
+
+gf.PhysicsSystem.prototype.findIdByEntity = function(ent) {
+    return this.findId(body, 0);
+};
+
+gf.PhysicsSystem.prototype.findIdByBody = function(body) {
+    return this.findId(body, 1);
+};
+
+gf.PhysicsSystem.prototype.findId = function(q, num) {
+    var idx = -1;
+
+    for(var i = 0, il = this.children.length; i < il; ++i) {
+        if(this.children[i][num] == q) {
+            idx = i;
+            break;
+        }
+    }
+
+    return idx;
+}
+
+gf.PhysicsSystem.prototype.update = function() {
+    this.world.step(this.game._delta);
+
+    //update each entity's position and rotation
+    for(var i = 0, il = this.children.length; i < il; ++i) {
+        var ent = this.children[i];
+
+        ent[0].setPosition(
+            p2.V.getX(ent[1].position),
+            p2.V.getY(ent[1].position)
+        );
+        ent[0].rotation = ent[1].angle;
+    }
+
+    //notify of any collisions, this seems really expensive (the id searches + multiple loops)!
+    for(var c = 0, cl = this.world.contacts.length; c < cl; ++c) {
+        var c = this.world.contacts[c],
+            biId = this.findIdByBody(c.bi),
+            bjId = this.findIdByBody(c.bj),
+            ent1 = this.children[biId][0],
+            ent2 = this.children[bjId][0];
+
+        ent1.onCollision(ent2);
+        ent2.onCollision(ent1);
+    }
 };
