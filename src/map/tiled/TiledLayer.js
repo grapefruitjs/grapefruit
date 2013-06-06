c5dcf6ff4a54703ee805801583d37c27fb472e33
Chad Engler <cengler@blizzard.com>
2013-06-05 17:57:31 -0700

    optimize the creation of the tile entities a bit

--- src/map/tiled/TiledLayer.js
+++ src/map/tiled/TiledLayer.js
@@ -69,36 +69,6 @@ gf.inherits(gf.TiledLayer, gf.Layer, {
         this._rendered.bottom = this._rendered.y + this._rendered.height;
     },
     /**
-     * Creates the sprite for a tile and caches it in a position
-     *
-     * @method getTileSprite
-     * @param tileX {Number} The x coord of the tile in units of tiles (not pixels)
-     * @param tileY {Number} The y coord of the tile in units of tiles (not pixels)
-     * @return {PIXI.Sprite} The sprite to display
-     */
-    getTileSprite: function(tileX, tileY) {
-        if(this.tiles[tileX] && this.tiles[tileX][tileY])
-            return this.tiles[tileX][tileY];
-
-        if(!this.tiles[tileX])
-            this.tiles[tileX] = {};
-
-        var id = (tileX + (tileY * this.size.x)),
-            tile = this.tileIds[id],
-            set = this.parent.getTileset(tile);
-
-        if(set) {
-            this.tiles[tileX][tileY] = new gf.Tile(this.game, [0, 0], {
-                texture: set.getTileTexture(tile),
-                mass: 999999,
-                collidable: false
-            });
-            this.addChild(this.tiles[tileX][tileY]);
-        }
-
-        return this.tiles[tileX][tileY];
-    },
-    /**
      * Moves a tile sprite from one position to another,
      * creating it if the old position didn't have a sprite
      *
@@ -110,31 +80,47 @@ gf.inherits(gf.TiledLayer, gf.Layer, {
      * @return {PIXI.Sprite} The sprite to display
      */
     moveTileSprite: function(fromTileX, fromTileY, toTileX, toTileY) {
-        var tile = this.getTileSprite(fromTileX, fromTileY);
-
-        if(!tile) return;
-
-        var id = (toTileX + (toTileY * this.size.x)),
+        var tile,
+            id = (toTileX + (toTileY * this.size.x)),
             tileId = this.tileIds[id],
-            set = this.parent.getTileset(tileId);
+            set = this.parent.getTileset(tileId),
+            texture,
+            props;
 
-        if(set) {
-            var props = set.getTileProperties(tileId);
+        if(!set) return;
 
-            tile.setTexture(set.getTileTexture(tileId));
-            tile.setCollidable(props.isCollidable);
+        var texture = set.getTileTexture(tileId),
+            props = set.getTileProperties(tileId),
+            position = [
+                toTileX * this.parent.tileSize.x,
+                toTileY * this.parent.tileSize.y
+            ];
+
+        //get the cached tile from the pool, and set the properties
+        if(this.tiles[fromTileX] && this.tiles[fromTileX][fromTileY]) {
+            tile = this.tiles[fromTileX][fromTileY];
+
             tile.collisionType = props.type;
+            tile.setTexture(texture);
+            tile.setCollidable(props.isCollidable);
+            tile.setPosition(position);
+
+            //move the sprite in the pool
+            if(!this.tiles[toTileX]) this.tiles[toTileX] = {};
+
+            this.tiles[toTileX][toTileY] = tile;
+            this.tiles[fromTileX][fromTileY] = null;
         }
-
-        tile.setPosition(
-            toTileX * this.parent.tileSize.x,
-            toTileY * this.parent.tileSize.y
-        );
-
-        //move the sprite in the pool
-        if(!this.tiles[toTileX]) this.tiles[toTileX] = {};
-        this.tiles[toTileX][toTileY] = tile;
-        this.tiles[fromTileX][fromTileY] = null;
+        //if there is no tile there yet, create one
+        else {
+            tile = this.tiles[toTileX][toTileY] = new gf.Tile(this.game, position, {
+                texture: texture,
+                mass: 999999,
+                collidable: props.isCollidable,
+                collisionType: props.type
+            });
+            this.addChild(tile);
+        }
 
         return tile;
     },
