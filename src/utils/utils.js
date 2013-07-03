6f50a5e1b74b724f664773ca180f54deaed3bf3e
Chad Engler <cengler@blizzard.com>
2013-07-02 17:38:45 -0700

    remove mouse code, should be using mouse events on sprites anyway

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -216,57 +216,5 @@
             val = parseInt(val, 10);
 
         return val;
-    },
-    //Some things stolen from jQuery, used for mouse input
-    getOffset: function(elem) {
-        var doc = elem && elem.ownerDocument,
-            docElem = doc.documentElement,
-            box;
-
-        try {
-            box = elem.getBoundingClientRect();
-        } catch(e) {}
-
-        // Make sure we're not dealing with a disconnected DOM node
-        if (!box || !(docElem !== elem && (docElem.contains ? docElem.contains(elem) : true))) {  //(!box || !jQuery.contains(docElem, elem)) {
-            return box ? {
-                top: box.top,
-                left: box.left
-            } : {
-                top: 0,
-                left: 0
-            };
-        }
-
-        var body = doc.body,
-            win = window,
-            clientTop = docElem.clientTop || body.clientTop || 0,
-            clientLeft = docElem.clientLeft || body.clientLeft || 0,
-            scrollTop = win.pageYOffset || docElem.scrollTop || body.scrollTop,
-            scrollLeft = win.pageXOffset || docElem.scrollLeft || body.scrollLeft,
-            top = box.top + scrollTop - clientTop,
-            left = box.left + scrollLeft - clientLeft;
-
-        return {
-            top: top,
-            left: left
-        };
-    },
-    ////////////////////////////////////////////////////////////////////////////////
-    // DOM Manipulation stuff will be removed with the GUI rewrite
-    getPosition: function(o) {
-        var l = o.offsetLeft,
-            t = o.offsetTop;
-
-        while(!!(o = o.offsetParent)) {
-            l += o.offsetLeft;
-            t += o.offsetTop;
-        }
-
-        return {
-            top: t,
-            left: l
-        };
     }
-    /////////////////////////////////////////////////////////////////////////////
 };
