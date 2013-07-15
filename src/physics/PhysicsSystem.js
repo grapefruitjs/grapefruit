6ee504c862df98dc721fac9d32f2730e3f51a4eb
Chad Engler <cengler@blizzard.com>
2013-07-15 13:43:23 -0700

    not sure why this used a different object method than everyone else

--- src/physics/PhysicsSystem.js
+++ src/physics/PhysicsSystem.js
@@ -1,8 +1,3 @@
-var COLLISION_TYPE = {
-    SPRITE: 0,
-    TILE: 1
-};
-
 gf.PhysicsSystem = function(options) {
     options = options || {};
 
@@ -22,8 +17,8 @@ gf.PhysicsSystem = function(options) {
 
     //sprite - sprite collisions
     this.space.addCollisionHandler(
-        COLLISION_TYPE.SPRITE,
-        COLLISION_TYPE.SPRITE,
+        gf.PhysicsSystem.COLLISION_TYPE.SPRITE,
+        gf.PhysicsSystem.COLLISION_TYPE.SPRITE,
         this.onCollisionBegin.bind(this), //begin
         null, //preSolve
         null, //postSolve
@@ -32,8 +27,8 @@ gf.PhysicsSystem = function(options) {
 
     //sprite - tile collisions
     this.space.addCollisionHandler(
-        COLLISION_TYPE.SPRITE,
-        COLLISION_TYPE.TILE,
+        gf.PhysicsSystem.COLLISION_TYPE.SPRITE,
+        gf.PhysicsSystem.COLLISION_TYPE.TILE,
         this.onCollisionBegin.bind(this), //begin
         null, //preSolve
         null, //postSolve
@@ -41,143 +36,140 @@ gf.PhysicsSystem = function(options) {
     );
 };
 
-gf.PhysicsSystem.prototype._createBody = function(spr) {
-    return this.space.addBody(new cp.Body(
-        spr.mass,
-        Infinity //cp.momsprForBox(spr.mass, spr.width, spr.height)
-    ));
-};
+gf.inherits(gf.PhysicsSystem, Object, {
+    _createBody: function(spr) {
+        return this.space.addBody(new cp.Body(
+            spr.mass,
+            Infinity //cp.momsprForBox(spr.mass, spr.width, spr.height)
+        ));
+    },
+    _createShape: function(spr, body) {
+        var shape = this.space.addShape(
+            new cp.BoxShape(
+                body,
+                spr.width,
+                spr.height
+            )
+        );
 
-gf.PhysicsSystem.prototype._createShape = function(spr, body) {
-    var shape = this.space.addShape(
-        new cp.BoxShape(
-            body,
-            spr.width,
-            spr.height
-        )
-    );
+        shape.setElasticity(0);
+        shape.setFriction(spr.friction || 0.1);
+        shape.sprite = spr;
+        shape.setCollisionType(this.getCollisionType(spr));
 
-    shape.setElasticity(0);
-    shape.setFriction(spr.friction || 0.1);
-    shape.sprite = spr;
-    shape.setCollisionType(this.getCollisionType(spr));
+        return shape;
+    },
+    getCollisionType: function(spr) {
+        if(spr instanceof gf.Tile) {
+            return gf.PhysicsSystem.COLLISION_TYPE.TILE;
+        } else {
+            return gf.PhysicsSystem.COLLISION_TYPE.SPRITE;
+        }
+    },
+    add: function(spr) {
+        if(!spr._phys)
+            spr._phys = {};
 
-    return shape;
-};
+        //already in system
+        if(spr._phys.body)
+            return;
 
-gf.PhysicsSystem.prototype.getCollisionType = function(spr) {
-    if(spr instanceof gf.Tile) {
-        return COLLISION_TYPE.TILE;
-    } else {
-        return COLLISION_TYPE.SPRITE;
+        var body = this._createBody(spr),
+            shape = this._createShape(spr, body);
+
+        spr._phys.body = body;
+        spr._phys.shape = shape;
+
+        //add control body and constraints
+        if(body.m !== Infinity) {
+            var cbody = new cp.Body(Infinity, Infinity), //control body
+                cpivot = this.space.addConstraint(new cp.PivotJoint(cbody, body, cp.vzero, cp.vzero)),
+                cgear = this.space.addConstraint(new cp.GearJoint(cbody, body, 0, 1));
+
+            cpivot.maxBias = 0; //disable join correction
+            cpivot.maxForce = 10000; //emulate linear friction
+
+            cgear.errorBias = 0; //attempt to fully correct the joint each step
+            cgear.maxBias = 1.2; //but limit the angular correction
+            cgear.maxForce = 50000; //emulate angular friction
+
+            if(!spr._phys.control)
+                spr._phys.control = {};
+
+            spr._phys.control.body = cbody;
+            spr._phys.control.pivot = cpivot;
+            spr._phys.control.gear = cgear;
+        }
+    },
+    remove: function(spr) {
+        if(!spr || !spr._phys || !spr._phys.body || !spr._phys.shape)
+            return;
+
+        this.space.remove(spr._phys.body);
+        this.space.remove(spr._phys.shape);
+
+        spr._phys.shape.sprite = null;
+
+        //remove references
+        spr._phys.body = null;
+        spr._phys.shape = null;
+    },
+    setMass: function(spr, mass) {
+        if(spr && spr._phys && spr._phys.body)
+            spr._phys.body.setMass(mass);
+    },
+    setVelocity: function(spr, vel) {
+        //update control body velocity (and pivot contraint makes regular follow)
+        if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
+            spr._phys.control.body.setVel(vel);
+
+        //if no control body then update real body
+        else if(spr && spr._phys && spr._phys.body)
+            spr._phys.body.setVel(vel);
+    },
+    setPosition: function(spr, pos) {
+        //update body position
+        if(spr && spr._phys && spr._phys.body)
+            spr._phys.body.setPos(pos);
+
+        //update control body position
+        if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
+            spr._phys.control.body.setPos(pos);
+    },
+    setRotation: function(spr, rads) {
+        //update control body rotation (and gear contraint makes regular follow)
+        if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
+            spr._phys.control.body.setAngle(rads);
+
+        //if no control body then update real body
+        else if(spr && spr._phys && spr._phys.body)
+            spr._phys.body.setAngle(rads);
+
+    },
+    update: function(dt) {
+        //execute the physics step
+        this.space.step(dt);
+
+        //go through each changed shape
+        this.space.activeShapes.each(function(shape) {
+            shape.sprite.setPosition(shape.body.p.x, shape.body.p.y, true);
+            shape.sprite.setRotation(shape.body.a, true);
+        });
+    },
+    onCollisionBegin: function(arbiter) {//, space) {
+        var shapes = arbiter.getShapes(),
+            spr1 = shapes[0].sprite,
+            spr2 = shapes[1].sprite;
+
+        spr1.onCollision(spr2);
+        spr2.onCollision(spr1);
+
+        //maintain the colliding state
+        return true;
     }
-};
+});
 
-gf.PhysicsSystem.prototype.add = function(spr) {
-    if(!spr._phys)
-        spr._phys = {};
-
-    //already in system
-    if(spr._phys.body)
-        return;
-
-    var body = this._createBody(spr),
-        shape = this._createShape(spr, body);
-
-    spr._phys.body = body;
-    spr._phys.shape = shape;
-
-    //add control body and constraints
-    if(body.m !== Infinity) {
-        var cbody = new cp.Body(Infinity, Infinity), //control body
-            cpivot = this.space.addConstraint(new cp.PivotJoint(cbody, body, cp.vzero, cp.vzero)),
-            cgear = this.space.addConstraint(new cp.GearJoint(cbody, body, 0, 1));
-
-        cpivot.maxBias = 0; //disable join correction
-        cpivot.maxForce = 10000; //emulate linear friction
-
-        cgear.errorBias = 0; //attempt to fully correct the joint each step
-        cgear.maxBias = 1.2; //but limit the angular correction
-        cgear.maxForce = 50000; //emulate angular friction
-
-        if(!spr._phys.control)
-            spr._phys.control = {};
-
-        spr._phys.control.body = cbody;
-        spr._phys.control.pivot = cpivot;
-        spr._phys.control.gear = cgear;
-    }
-};
-
-gf.PhysicsSystem.prototype.remove = function(spr) {
-    if(!spr || !spr._phys || !spr._phys.body || !spr._phys.shape)
-        return;
-
-    this.space.remove(spr._phys.body);
-    this.space.remove(spr._phys.shape);
-
-    spr._phys.shape.sprite = null;
-
-    //remove references
-    spr._phys.body = null;
-    spr._phys.shape = null;
-};
-
-gf.PhysicsSystem.prototype.setMass = function(spr, mass) {
-    if(spr && spr._phys && spr._phys.body)
-        spr._phys.body.setMass(mass);
-};
-
-gf.PhysicsSystem.prototype.setVelocity = function(spr, vel) {
-    //update control body velocity (and pivot contraint makes regular follow)
-    if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
-        spr._phys.control.body.setVel(vel);
-
-    //if no control body then update real body
-    else if(spr && spr._phys && spr._phys.body)
-        spr._phys.body.setVel(vel);
-};
-
-gf.PhysicsSystem.prototype.setPosition = function(spr, pos) {
-    //update body position
-    if(spr && spr._phys && spr._phys.body)
-        spr._phys.body.setPos(pos);
-
-    //update control body position
-    if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
-        spr._phys.control.body.setPos(pos);
-};
-
-gf.PhysicsSystem.prototype.setRotation = function(spr, rads) {
-    //update control body rotation (and gear contraint makes regular follow)
-    if(spr && spr._phys && spr._phys.control && spr._phys.control.body)
-        spr._phys.control.body.setAngle(rads);
-
-    //if no control body then update real body
-    else if(spr && spr._phys && spr._phys.body)
-        spr._phys.body.setAngle(rads);
-
-};
-
-gf.PhysicsSystem.prototype.update = function(dt) {
-    //execute the physics step
-    this.space.step(dt);
-
-    //go through each changed shape
-    this.space.activeShapes.each(function(shape) {
-        shape.sprite.setPosition(shape.body.p.x, shape.body.p.y, true);
-        shape.sprite.setRotation(shape.body.a, true);
-    });
-};
-
-gf.PhysicsSystem.prototype.onCollisionBegin = function(arbiter) {//, space) {
-    var shapes = arbiter.getShapes(),
-        spr1 = shapes[0].sprite,
-        spr2 = shapes[1].sprite;
-
-    spr1.onCollision(spr2);
-    spr2.onCollision(spr1);
-
-    //maintain the colliding state
-    return true;
+gf.PhysicsSystem.COLLISION_TYPE = {
+    SPRITE: 0,
+    TILE: 1
 };
