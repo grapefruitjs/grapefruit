3a42c9546557b7441df659c6a9ced13f101c494a
Chad Engler <englercj@live.com>
2013-07-16 21:04:33 -0700

    add a v to verison

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,7 +223,7 @@ module.exports = function(grunt) {
             compile: {
                 name: '<%= pkg.longName %>',
                 description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
+                version: 'v<%= pkg.version %>',
                 url: '<%= pkg.homepage %>',
                 logo: 'https://en.gravatar.com/userimage/49218683/8654d0c767f327312ebb1ace7f5a8d8d.png',
                 options: {
