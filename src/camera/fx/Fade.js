b5a977f70a364f239addbe98232c8d94e33b5ed2
Chad Engler <cengler@blizzard.com>
2013-08-01 14:45:21 -0700

    fix issues with camera

--- src/camera/fx/Fade.js
+++ src/camera/fx/Fade.js
@@ -2,29 +2,30 @@ gf.Camera.fx.Fade = function() {
     gf.Camera.fx.Effect.call(this);
 };
 
-gf.inherits(gf.Camera.fx.Fade, gf.DisplayObjectContainer, {
+gf.inherits(gf.Camera.fx.Fade, gf.Camera.fx.Effect, {
     start: function(color, duration, cb) {
         gf.Camera.fx.Effect.prototype.start.call(this);
 
         if(typeof duration === 'function') {
             cb = duration;
-            duration = 1;
+            duration = null;
         }
 
         if(typeof color === 'function') {
             cb = color;
-            duration = 1;
-            color = 0xFFFFFF;
+            duration = null;
+            color = null;
         }
 
-        this.duration = duration && duration > 0 ? duration : 1;
+        color = typeof color === 'number' ? color : 0xFFFFFF;
+        this.duration = duration && duration > 0 ? duration : 1000;
         this.cb = cb;
 
         this.gfx.visible = true;
         this.gfx.alpha = 0;
         this.gfx.clear();
-        this.gfx.fillStyle = color !== undefined ? color : 0xFFFFFF;
-        this.gfx.fillRect(0, 0, this.parent.size.x, this.parent.size.y);
+        this.gfx.beginFill(color);
+        this.gfx.drawRect(0, 0, this.parent.size.x, this.parent.size.y);
 
         return this;
     },
@@ -37,7 +38,7 @@ gf.inherits(gf.Camera.fx.Fade, gf.DisplayObjectContainer, {
         return this;
     },
     update: function(dt) {
-        if(this.gfx.alpha <= 0) {
+        if(this.gfx.alpha < 1) {
             this.gfx.alpha += (dt * 1000) / this.duration;
 
             if(this.gfx.alpha >= 1) {
--- src/camera/fx/Flash.js
+++ src/camera/fx/Flash.js
@@ -2,29 +2,30 @@ gf.Camera.fx.Flash = function() {
     gf.Camera.fx.Effect.call(this);
 };
 
-gf.inherits(gf.Camera.fx.Flash, gf.DisplayObjectContainer, {
+gf.inherits(gf.Camera.fx.Flash, gf.Camera.fx.Effect, {
     start: function(color, duration, cb) {
         gf.Camera.fx.Effect.prototype.start.call(this);
 
         if(typeof duration === 'function') {
             cb = duration;
-            duration = 1;
+            duration = null;
         }
 
         if(typeof color === 'function') {
             cb = color;
-            duration = 1;
-            color = 0xFFFFFF;
+            duration = null;
+            color = null;
         }
 
-        this.duration = duration && duration > 0 ? duration : 1;
+        color = typeof color === 'number' ? color : 0xFFFFFF;
+        this.duration = duration && duration > 0 ? duration : 1000;
         this.cb = cb;
 
         this.gfx.visible = true;
         this.gfx.alpha = 1;
         this.gfx.clear();
-        this.gfx.fillStyle = color !== undefined ? color : 0xFFFFFF;
-        this.gfx.fillRect(0, 0, this.parent.size.x, this.parent.size.y);
+        this.gfx.beginFill(color);
+        this.gfx.drawRect(0, 0, this.parent.size.x, this.parent.size.y);
 
         return this;
     },
