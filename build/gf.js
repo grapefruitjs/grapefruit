2141824d1776484ddcca9eaed6b07e0778d43c43
Chad Engler <englercj@live.com>
2013-09-01 11:57:28 -0700

    new builds

--- build/gf.js
+++ build/gf.js
@@ -22781,7 +22781,8 @@ gf.input.GP_BUTTON = {
     PAD_TOP: 12, // Directional (discrete) pad
     PAD_BOTTOM: 13,
     PAD_LEFT: 14,
-    PAD_RIGHT: 15
+    PAD_RIGHT: 15,
+    SYSTEM_MENU: 16   // on console controllers this would be the button to open the system menu
 };
 gf.input.getGpButtonName = function(i) {
     for(var k in gf.input.GP_BUTTON) {
@@ -22855,6 +22856,7 @@ gf.inherits(gf.input.GamepadButtons, gf.input.Input, {
         }
     }
 });
+
 /**
  * Bindable Gamepad Axes
  *
