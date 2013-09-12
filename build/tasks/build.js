bf9048e770915b35fb02c2fec7903710aa897583
Chad Engler <cengler@blizzard.com>
2013-09-12 12:04:28 -0700

    change build processes slightly

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -3,45 +3,40 @@
 module.exports = function(grunt) {
     var fs = require('fs'),
         flags = this.flags,
-        version = grunt.config('pkg.version');
+        version = grunt.config('pkg.version'),
+        modules = grunt.file.readJSON('build/modules.json');
 
     grunt.registerMultiTask('build', 'Create a distributable build of grapefruit', function() {
-        var modules = grunt.file.readJSON('build/modules.json'),
-            bundle = 'module.exports={';
+        var flags = this.flags,
+            bundle;
 
-        console.log(this);
+        if(flags['*']) {
+            bundle = getModuleBundle(modules);
+        } else {
+            if(!flags.core)
+                flags.core = true;
 
-        if(!this.flags.core)
-            this.flags.core = true;
+            bundle = getModuleBundle(flags);
+        }
 
-        Object.keys(this.flags).forEach(function(f) {
-            if(modules[f]) {
-                bundle += f + ':require("' + f + '/' + f + '"),';
-            }
-        });
-
-        bundle = bundle.substr(0, bundle.length - 1);
-        bundle += '};';
-
-        grunt.file.write('src/bundle.js', bundle);
+        grunt.file.write(this.data.main, bundle);
 
         //run urequire
         grunt.task.run([ 'urequire:' + this.target ]);
     });
 
-    // Special "alias" task to make custom build creation less grawlix-y
-    // Translation example
-    //
-    //   grunt custom:+ajax,-dimensions,-effects,-offset
-    //
-    // Becomes:
-    //
-    //   grunt build:*:*:+ajax:-dimensions:-effects:-offset
-    grunt.registerTask('custom', function() {
-        var args = [].slice.call(arguments),
-            modules = args.length ? args[0].replace(/,/g, ':') : '';
+    function getModuleBundle(mods) {
+        var str = 'module.exports={';
 
-        grunt.log.writeln('Creating custom build...\n');
-        grunt.task.run([ 'build:*:*:' + modules ]);
-    });
+        Object.keys(mods).forEach(function(f) {
+            if(modules[f]) {
+                str += f + ':require("' + f + '/' + f + '"),';
+            }
+        });
+
+        str = str.substr(0, str.length - 1);
+        str += '};';
+
+        return str;
+    }
 };
