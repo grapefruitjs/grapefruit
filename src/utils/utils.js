81095892da45ed76eebfd55751c7cdf3f21e6f89
Chad Engler <cengler@blizzard.com>
2013-09-26 15:43:08 -0700

    Add extend/isPlainObject utils, and Sprite.clone

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -210,6 +210,108 @@ var utils = module.exports = {
 
         return obj;
     },
+    /**
+     * From jQuery.extend, extends one object into another
+     * taken straight from jQuery
+     *
+     * @method extend
+     */
+    extend: function() {
+        var src, copyIsArray, copy, name, options, clone, target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if (typeof target === "boolean") {
+            deep = target;
+            target = arguments[1] || {};
+            // skip the boolean and the target
+            i = 2;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if (typeof target !== "object" && typeof target !== 'function') {
+            target = {};
+        }
+
+        for (; i < length; i++) {
+            // Only deal with non-null/undefined values
+            if ((options = arguments[i]) != null) {
+                // Extend the base object
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+
+                    // Prevent never-ending loop
+                    if (target === copy) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if (deep && copy && (utils.isPlainObject(copy) || copyIsArray = Array.isArray(copy))) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && Array.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && utils.isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[name] = utils.extend(deep, clone, copy);
+
+                        // Don't bring in undefined values
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    },
+    /**
+     * From jQuery.isPlainObject, checks if an object is a plain object
+     * taken straight from jQuery
+     *
+     * @method isPlainObject
+     */
+    isPlainObject: function(obj) {
+        var key;
+
+        // Must be an Object.
+        // Because of IE, we also have to check the presence of the constructor property.
+        // Make sure that DOM nodes and window objects don't pass through, as well
+        if (!obj || typeof obj !== "object" || obj.nodeType || obj == obj.window) {
+            return false;
+        }
+
+        try {
+            // Not own constructor property must be Object
+            if (obj.constructor && !Object.hasOwnProperty.call(obj, 'constructor') && !Object.hasOwnProperty.call(obj.constructor.prototype, 'isPrototypeOf')) {
+                return false;
+            }
+        } catch(e) {
+            // IE8,9 Will throw exceptions on certain host objects #9897
+            return false;
+        }
+
+        // Support: IE<9
+        // Handle iteration over inherited properties before own properties.
+        if (support.ownLast) {
+            for (key in obj) {
+                return Object.hasOwnProperty.call(obj, key);
+            }
+        }
+
+        // Own properties are enumerated firstly, so to speed up,
+        // if last one is own, then all properties are own.
+        for (key in obj) {}
+
+        return key === undefined || hasOwn.call(obj, key);
+    },
     parseHitArea: function(hv) {
         var ha;
 
