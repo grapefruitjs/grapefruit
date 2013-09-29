956a8a88f230825a304509f29087eb0a9df98838
Chad Engler <englercj@live.com>
2013-09-29 10:01:14 -0700

    beef up some math and shapes

--- src/math/Circle.js
+++ src/math/Circle.js
@@ -1,1 +1,75 @@
-module.exports = require('../vendor/pixi').Circle;
+var inherit = require('../utils/inherit');
+
+/**
+ * The Circle object is an area defined by its position, as indicated by its
+ * center point (x, y) and by its radius.
+ *
+ * @class Circle
+ * @constructor
+ * @param x {Number} The X coord of the center of the circle
+ * @param y {Number} The Y coord of the center of the circle
+ * @param radius {Number} The radius of the circle
+ */
+var Circle = module.exports = function(x, y, radius) {
+    /**
+     * The center X coord of the circle
+     *
+     * @property x
+     * @type Number
+     * @default 0
+     */
+    this.x = x || 0;
+    
+    /**
+     * The center Y coord of the circle
+     *
+     * @property y
+     * @type Number
+     * @default 0
+     */
+    this.y = y || 0;
+
+    /**
+     * The radius of the circle
+     *
+     * @property radius
+     * @type Number
+     * @default 0
+     */
+    this.radius = radius || 0;
+};
+
+inherit(Circle, Object, {
+    /**
+     * Creates a clone of this Circle instance
+     *
+     * @method clone
+     * @return {Circle} a copy of the polygon
+     */
+    clone: function() {
+        return new Circle(this.x, this.y, this.radius);
+    },
+
+    /**
+     * Checks if the x, and y coords passed to this function are contained within this circle,
+     * or on the edge of the circle
+     *
+     * @method contains
+     * @param x {Number} The X coord of the point to test
+     * @param y {Number} The Y coord of the point to test
+     * @return {Boolean} if the x/y coords are within this polygon
+     */
+    contains: function(x, y) {
+        if(this.radius <= 0)
+            return false;
+
+        var dx = (x - this.x),
+            dy = (y - this.y),
+            r2 = this.radius * this.radius;
+
+        dx *= dx;
+        dy *= dy;
+
+        return (dx + dy <= r2);
+    }
+});
--- src/math/Polygon.js
+++ src/math/Polygon.js
@@ -1,1 +1,151 @@
-module.exports = require('../vendor/pixi').Polygon;
+var inherit = require('../utils/inherit');
+
+/**
+ * A *convex* clockwise Polygon.
+ *
+ * @class Polygon
+ * @constructor
+ * @param x {Number} The X origin of the polygon, all X coords for all points are relative to this
+ * @param y {Number} The Y origin of the polygon, all Y coords for all points are relative to this
+ * @param points* {Array<Vector>|Array<Number>|Point...|Number...} This can be an array of Vectors that form the polygon,
+ *      a flat array of numbers that will be interpreted as [x,y, x,y, ...], or the arugments passed can be
+ *      all the points of the polygon e.g. `new Polygon(X, Y, new Vector(), new Vector(), ...)`, or the
+ *      arguments passed can be flat x,y values e.g. `new Polygon(X, Y, x,y, x,y, x,y, ...)` where `x` and `y` are
+ *      Numbers.
+ */
+var Polygon = module.exports = function(x, y, points) {
+    /**
+     * The origin X coord of the polygon
+     *
+     * @property x
+     * @type Number
+     * @default 0
+     */
+    this.x = x || 0;
+
+    /**
+     * The origin Y coord of the polygon
+     *
+     * @property x
+     * @type Number
+     * @default 0
+     */
+    this.y = y || 0;
+
+    /**
+     * The points of the polygon, the X & Y values here should be
+     * relative to the origin X & Y values.
+     *
+     * @property points
+     * @type Array<Vector>
+     * @default []
+     */
+    this.points = null;
+
+    /**
+     * These vectors are calculated by `this.recalc()` and represent the edges
+     * of the polygon defined by it's points.
+     *
+     * @property edges
+     * @type Array<Vector>
+     * @default []
+     * @readOnly
+     */
+    this.edges = [];
+
+    /**
+     * These vectors are calculated by `this.recalc()` and represent the normals
+     * of the polygon edges defined by it's points.
+     *
+     * @property normals
+     * @type Array<Vector>
+     * @default []
+     * @readOnly
+     */
+    this.normals = [];
+
+    //if points isn't an array, use arguments as the array
+    if(!(points instanceof Array))
+        points = Array.prototype.slice.call(arguments, 2);
+
+    //if this is a flat array of numbers, convert it to points
+    if(typeof points[0] === 'number') {
+        var p = [];
+        for(var i = 0, il = points.length; i < il; i+=2) {
+            p.push(
+                new Vector(points[i], points[i + 1])
+            );
+        }
+
+        points = p;
+    }
+
+    this.points = points;
+
+    //recalculate edges and normals
+    this.recalc();
+};
+
+inherit(Polygon, Object, {
+    /**
+     * Creates a clone of this polygon
+     *
+     * @method clone
+     * @return {Polygon} a copy of the polygon
+     */
+    clone: function() {
+        var points = [];
+        for (var i=0; i<this.points.length; i++) {
+            points.push(this.points[i].clone());
+        }
+
+        return new Polygon(points);
+    }
+
+    /**
+     * Checks if the x, and y coords passed to this function are contained within this polygon
+     *
+     * @method contains
+     * @param x {Number} The X coord of the point to test
+     * @param y {Number} The Y coord of the point to test
+     * @return {Boolean} if the x/y coords are within this polygon
+     */
+    contains: function(x, y) {
+        var inside = false;
+
+        // use some raycasting to test hits
+        // https://github.com/substack/point-in-polygon/blob/master/index.js
+        for(var i = 0, j = this.points.length - 1; i < this.points.length; j = i++) {
+            var xi = this.points[i].x, yi = this.points[i].y,
+                xj = this.points[j].x, yj = this.points[j].y,
+                intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+
+            if(intersect) inside = !inside;
+        }
+
+        return inside;
+    },
+
+    /**
+     * Recalculates the edges and normals of this polygon based on the points
+     *
+     * @method recalc
+     */
+    recalc: function() {
+        var points = this.points,
+            len = points.length,
+            p1, p2, e, n;
+
+        this.edges.length = this.normals.length = 0;
+
+        for(var i = 0; i < len; ++i) {
+            p1 = points[i];
+            p2 = i < len - 1 ? points[i + 1] : points[0];
+            e = p2.clone().sub(p1);
+            n = e.clone().perp().normalize();
+
+            this.edges.push(e);
+            this.normals.push(n);
+        }
+    }
+});
