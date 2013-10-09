91fe79c42d3832f48d600825c513111e525dc148
Chad Engler <englercj@live.com>
2013-10-09 07:41:45 -0700

    Move jshint options to .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,42 +54,7 @@ module.exports = function(grunt) {
         jshint: {
             src: source.concat('Gruntfile.js'),
             options: {
-                /* Enforcement options */
-                bitwise: false,     //allow bitwise operators
-                camelcase: false,   //must use camelCase or UPPER_CASE
-                curly: false,       //one line conditionals w/o braces are allowed
-                eqeqeq: true,       //must use === if possible
-                forin: false,       //forin loops much check hasOwnProperty
-                immed: true,        //self-calling functions must be wrapped in parens
-                latedef: true,      //can't use a variable until it is defined
-                newcap: true,       //ctor names must be Captialized
-                noarg: true,        //arguments.caller/callee are deprecated, disallow
-                noempty: true,      //warn about empty blocks
-                nonew: true,        //no using `new Constructor();` without saving the value (no using only side-effects)
-                plusplus: false,    //you can use unary increment and decrement operators
-                quotmark: true,     //quotes must be consistent
-                unused: true,       //warn about declared but not used variables
-                strict: false,      //do not require functions to be able to run in strict-mode
-                trailing: true,     //help prevent weird whitespace errors in multi-line strings using \ 
-                maxlen: 200,        //no line should be longer than 200 characters
-
-                /* Relaxing Options */
-                boss: true,         //do not warn about the use of assignments in cases where comparisons are expected (eg: `return this.types[name] = obj;` is OK)
-                expr: true,         //do not warn about expressions as programs (mainly for urequire root exports in core.js)
-
-                /* Environments */
-                browser: true,      //this runs in a browser :)
-                devel: false,       //warn about using console.log and the like
-                jquery: false,      //no jquery used here
-                node: false,        //no node support...YET! :)
-                worker: true,       //web-workers are used
-
-                /* Globals */
-                undef: true,
-                globals: {
-                    module: false,
-                    require: false
-                }
+                jshintrc: '.jshintrc'
             }
         },
         connect: {
