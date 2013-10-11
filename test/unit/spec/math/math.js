1d13826caf9eb140484edcf65f3f2a9748b01355
Chad Engler <cengler@blizzard.com>
2013-10-11 13:16:29 -0700

    add a test for random object element

--- test/unit/spec/math/math.js
+++ test/unit/spec/math/math.js
@@ -462,5 +462,11 @@ describe('math', function() {
 
             a.should.contain(gf.math.randomElement(a));
         });
+
+        it('should return a random object element from an array', function() {
+            var a = [{k:1},{k:2},{k:3},{k:4},{k:5},{k:6},{k:7},{k:8}];
+
+            a.should.contain(gf.math.randomElement(a));
+        });
     });
 });
