954d237ad69df5c28461e9070e9721e9bd669901
Chad Engler <englercj@live.com>
2013-10-11 21:02:08 -0700

    Add some whitespace to better format core exports

--- src/core.js
+++ src/core.js
@@ -31,93 +31,93 @@
 
 module.exports = {
     //audio
-    AudioManager:   require('./audio/AudioManager'),
-    AudioPlayer:    require('./audio/AudioPlayer'),
+    AudioManager:       require('./audio/AudioManager'),
+    AudioPlayer:        require('./audio/AudioPlayer'),
 
     //camera
-    Camera: require('./camera/Camera'),
+    Camera:             require('./camera/Camera'),
 
     //display
-    BaseTexture:    require('./display/BaseTexture'),
-    Texture:        require('./display/Texture'),
-    Container:      require('./display/Container'),
-    Sprite:         require('./display/Sprite'),
+    BaseTexture:        require('./display/BaseTexture'),
+    Texture:            require('./display/Texture'),
+    Container:          require('./display/Container'),
+    Sprite:             require('./display/Sprite'),
 
     //font
-    BitmapText:     require('./text/BitmapText'),
+    BitmapText:         require('./text/BitmapText'),
 
     //fx
     fx: {
         camera: {
-            Effect: require('./fx/camera/Effect'),
-            Close:  require('./fx/camera/Close'),
-            Fade:   require('./fx/camera/Fade'),
-            Flash:  require('./fx/camera/Flash'),
-            Scanlines: require('./fx/camera/Scanlines'),
-            Shake:  require('./fx/camera/Shake')
+            Effect:     require('./fx/camera/Effect'),
+            Close:      require('./fx/camera/Close'),
+            Fade:       require('./fx/camera/Fade'),
+            Flash:      require('./fx/camera/Flash'),
+            Scanlines:  require('./fx/camera/Scanlines'),
+            Shake:      require('./fx/camera/Shake')
         }
     },
 
     //game
-    Game:       require('./game/Game'),
-    State:      require('./game/State'),
-    StateManager: require('./game/StateManager'),
+    Game:               require('./game/Game'),
+    State:              require('./game/State'),
+    StateManager:       require('./game/StateManager'),
 
     //gui
-    GuiItem:    require('./gui/GuiItem'),
+    GuiItem:            require('./gui/GuiItem'),
 
     //input
-    Input:          require('./input/Input'),
-    InputManager:   require('./input/InputManager'),
-    Keyboard:       require('./input/Keyboard'),
-    Gamepad:        require('./input/Gamepad'),
-    GamepadButtons: require('./input/gamepad/GamepadButtons'),
-    GamepadSticks:  require('./input/gamepad/GamepadSticks'),
-    Pointers:       require('./input/Pointers'),
-    Pointer:        require('./input/pointer/Pointer'),
+    Input:              require('./input/Input'),
+    InputManager:       require('./input/InputManager'),
+    Keyboard:           require('./input/Keyboard'),
+    Gamepad:            require('./input/Gamepad'),
+    GamepadButtons:     require('./input/gamepad/GamepadButtons'),
+    GamepadSticks:      require('./input/gamepad/GamepadSticks'),
+    Pointers:           require('./input/Pointers'),
+    Pointer:            require('./input/pointer/Pointer'),
 
     //loader
-    Loader: require('./loader/Loader'),
+    Loader:             require('./loader/Loader'),
 
     //map
-    Tile:       require('./tilemap/Tile'),
-    Tilelayer:  require('./tilemap/Tilelayer'),
-    Tilemap:    require('./tilemap/Tilemap'),
-    Tileset:    require('./tilemap/Tileset'),
-    ObjectGroup: require('./tilemap/ObjectGroup'),
+    Tile:               require('./tilemap/Tile'),
+    Tilelayer:          require('./tilemap/Tilelayer'),
+    Tilemap:            require('./tilemap/Tilemap'),
+    Tileset:            require('./tilemap/Tileset'),
+    ObjectGroup:        require('./tilemap/ObjectGroup'),
 
     //math
-    math:       require('./math/math'),
-    Circle:     require('./geom/Circle'),
-    Ellipse:    require('./geom/Ellipse'),
-    Polygon:    require('./geom/Polygon'),
-    Rectangle:  require('./geom/Rectangle'),
-    Vector:     require('./math/Vector'),
+    math:               require('./math/math'),
+    Circle:             require('./geom/Circle'),
+    Ellipse:            require('./geom/Ellipse'),
+    Polygon:            require('./geom/Polygon'),
+    Rectangle:          require('./geom/Rectangle'),
+    Vector:             require('./math/Vector'),
 
     //particles
     ParticleEmitter:    require('./particles/ParticleEmitter'),
     ParticleSystem:     require('./particles/ParticleSystem'),
 
     //physics
-    Physics:    require('./physics/Physics'),
-    Body:       require('./physics/Body'),
+    Physics:            require('./physics/Physics'),
+    Body:               require('./physics/Body'),
 
     //utils
-    utils:          require('./utils/utils'),
-    support:        require('./utils/support'),
-    inherit:        require('./utils/inherit'),
-    Cache:          require('./utils/Cache'),
-    Clock:          require('./utils/Clock'),
-    EventEmitter:   require('./utils/EventEmitter'),
-    ObjectPool:     require('./utils/ObjectPool'),
-    SpritePool:     require('./utils/SpritePool'),
-    ObjectFactory:  require('./utils/ObjectFactory'),
+    utils:              require('./utils/utils'),
+    support:            require('./utils/support'),
+    inherit:            require('./utils/inherit'),
+    Cache:              require('./utils/Cache'),
+    Clock:              require('./utils/Clock'),
+    EventEmitter:       require('./utils/EventEmitter'),
+    ObjectPool:         require('./utils/ObjectPool'),
+    SpritePool:         require('./utils/SpritePool'),
+    ObjectFactory:      require('./utils/ObjectFactory'),
 
     //plugin
-    plugin: require('./plugin'),
+    plugin:             require('./plugin'),
 
     //vendor files
-    PIXI:   require('./vendor/pixi')
+    PIXI:               require('./vendor/pixi')
 };
 
 //copy over constants
