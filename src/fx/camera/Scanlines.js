84166c6ddf96dd9d2741352825fcf73956ad8b0b
Chad Engler <englercj@live.com>
2013-11-03 17:43:31 -0800

    fix issues with camera fx

--- src/fx/camera/Scanlines.js
+++ src/fx/camera/Scanlines.js
@@ -7,14 +7,51 @@ var Scanlines = function() {
 };
 
 inherit(Scanlines, Effect, {
-    start: function(color, direction, spacing, thickness, alpha) {
+    start: function(color, axis, spacing, thickness, alpha, cb) {
         Effect.prototype.start.call(this);
 
+        if(typeof alpha ==='function') {
+            cb = alpha;
+            alpha = null;
+        }
+
+        if(typeof thickness === 'function') {
+            cb = thickness;
+            alpha = null;
+            thickness = null;
+        }
+
+        if(typeof spacing === 'function') {
+            cb = spacing;
+            alpha = null;
+            thickness = null;
+            spacing = null;
+        }
+
+        if(typeof axis === 'function') {
+            cb = spacing;
+            alpha = null;
+            thickness = null;
+            spacing = null;
+            axis = null;
+        }
+
+        if(typeof color === 'function') {
+            cb = spacing;
+            alpha = null;
+            thickness = null;
+            spacing = null;
+            axis = null;
+            color = null;
+        }
+
         color = color || 0x000000;
-        direction = direction || C.AXIS.HORIZONTAL;
+        axis = axis || C.AXIS.HORIZONTAL;
         spacing = spacing || 4;
         thickness = thickness || 1;
         alpha = alpha || 0.3;
+
+        this.cb = cb;
 
         var sx = this.parent.size.x,
             sy = this.parent.size.y;
@@ -24,13 +61,13 @@ inherit(Scanlines, Effect, {
         this.gfx.beginFill(color, alpha);
 
         //draw the lines
-        if(direction & C.AXIS.VERTICAL) {
+        if(axis & C.AXIS.VERTICAL) {
             for(var x = 0; x < sx; x += spacing) {
                 this.gfx.drawRect(x, 0, thickness, sy);
             }
         }
 
-        if(direction & C.AXIS.HORIZONTAL) {
+        if(axis & C.AXIS.HORIZONTAL) {
             for(var y = 0; y < sy; y += spacing) {
                 this.gfx.drawRect(0, y, sx, thickness);
             }
