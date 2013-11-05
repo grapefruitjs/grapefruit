fed553648b9aefe72e47ec2b9ab63a877a546147
Chad Engler <cengler@blizzard.com>
2013-11-05 09:29:37 -0800

    organize xml parser a bit

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -405,31 +405,34 @@ var utils = {
     }
 };
 
-//XML Parser
+//XML Parser in window
 if(typeof window.DOMParser !== 'undefined') {
     utils.parseXML = function(xmlStr) {
         return (new window.DOMParser()).parseFromString(xmlStr, 'text/xml');
     };
-} else if(typeof window.ActiveXObject !== 'undefined' && new window.ActiveXObject('Microsoft.XMLDOM')) {
+}
+//IE specific XML parser
+else if(typeof window.ActiveXObject !== 'undefined' && new window.ActiveXObject('Microsoft.XMLDOM')) {
     utils.parseXML = function(xmlStr) {
         var xmlDoc = new window.ActiveXObject('Microsoft.XMLDOM');
         xmlDoc.async = 'false';
         xmlDoc.loadXML(xmlStr);
         return xmlDoc;
     };
-} else {
-    //node.js environment
-    /*if(__isNode) {
-        utils.parseXML = function(xmlStr) {
-            var DOMParser = require('xmldom').DOMParser;
-            return (new DOMParser()).parseFromString(xmlStr, "text/xml");
-        };
-    } else {*/
+}
+//node.js environment
+/*else if(__isNode) {
+    utils.parseXML = function(xmlStr) {
+        var DOMParser = require('xmldom').DOMParser;
+        return (new DOMParser()).parseFromString(xmlStr, "text/xml");
+    };
+}*/
+// no parser available
+else {
     utils.warn('XML parser not available, trying to parse any XML will result in an error.');
     utils.parseXML = function() {
         throw new Error('Trying to parse XML, but not XML parser is available in this environment');
     };
-    //}
 }
 
 module.exports = utils;
