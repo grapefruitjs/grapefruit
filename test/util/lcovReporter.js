458885c3ff99271390ddd0e9491eda5d9ab46385
Chad Engler <cengler@blizzard.com>
2014-01-22 13:59:47 -0800

    Add coverage reporting to tests

---
+++ test/util/lcovReporter.js
@@ -0,0 +1,34 @@
+(function() {
+    function getLcov(filename, data) {
+        //generate lcov info
+        var str = 'SF:' + filename + '\n';
+
+        data.source.forEach(function(line, num) {
+            // increase the line number, as JS arrays are zero-based
+            num++;
+
+            if (data[num] !== undefined) {
+                str += 'DA:' + num + ',' + data[num] + '\n';
+            }
+       });
+
+       str += 'end_of_record\n';
+
+       return str;
+    }
+
+    function sendMessage() {
+        if(!window.PHANTOMJS) return;
+        var args = [].slice.call(arguments);
+        alert(JSON.stringify(args));
+    }
+
+    blanket.customReporter = function(coverageData) {
+        for(var filename in coverageData.files) {
+            var data = coverageData.files[filename];
+            sendMessage('blanket.fileDone', getLcov(filename, data));
+        }
+
+        sendMessage('blanket.done');
+    };
+})();
