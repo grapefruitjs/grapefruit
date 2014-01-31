c42fdb45bcdaf03be6eed16828010d67b08d3345
Chad Engler <cengler@blizzard.com>
2014-01-31 15:41:30 -0800

    Add Queue data structure

---
+++ src/utils/Queue.js
@@ -0,0 +1,42 @@
+var inherit = require('./inherit');
+
+var Queue = function() {
+    this.data = [];
+    this.index = 0;
+};
+
+inherit(Queue, Object, {
+    enqueue: function(item) {
+        this.data.push(item);
+    },
+
+    dequeue: function() {
+        if(this.data.length === 0)
+            return;
+
+        var item = this.data[this.index];
+
+        if(++this.index * 2 >= this.data.length) {
+            this.data = this.data.slice(this.index);
+            this.index = 0;
+        }
+
+        return item;
+    },
+
+    peek: function() {
+        return (this.data.length > 0 ? this.data[this.index] : undefined);
+    }
+});
+
+Object.defineProperty(Queue.prototype, 'length', {
+    get: function() {
+        return (this.data.length - this.index);
+    }
+});
+
+Object.defineProperty(Queue.prototype, 'empty', {
+    get: function() {
+        return (this.data.length === 0);
+    }
+});
